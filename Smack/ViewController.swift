25ed5c72ea70a214306f1c945d872f0e0dd2f76a
Steven <stevenalfred93@yahoo.com>
2018-10-03 00:12:08 -0400

    Initial Commit

---
+++ Smack/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Smack
+//
+//  Created by Steven on 10/3/18.
+//  Copyright © 2018 Steven Santiago. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
