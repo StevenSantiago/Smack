3023f27c338a6103dcdbd67bcd454ab1c294f5c6
Steven <stevenalfred93@yahoo.com>
2018-10-04 20:42:06 -0400

    Added Login Screen

---
+++ Smack/Controller/LoginVC.swift
@@ -0,0 +1,24 @@
+//
+//  LoginVC.swift
+//  Smack
+//
+//  Created by Steven on 10/4/18.
+//  Copyright © 2018 Steven Santiago. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func closePressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+
+
+}
---
+++ Smack/Utilities/Constants.swift
@@ -0,0 +1,11 @@
+//
+//  Constants.swift
+//  Smack
+//
+//  Created by Steven on 10/4/18.
+//  Copyright © 2018 Steven Santiago. All rights reserved.
+//
+
+import Foundation
+
+let TO_LOGIN = "toLogin"
